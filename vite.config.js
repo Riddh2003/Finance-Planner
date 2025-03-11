6747bc0cdca2ccb4afa4ddb59cce2b3fe69378d2
Riddh Modi <riddhmodi2003@gmail.com>
2025-03-11 09:25:02 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,10 @@ export default defineConfig({
   server: {
     host: true, // Needed for docker/render.com
     strictPort: true,
-    port: process.env.PORT || 3000 // Use PORT from environment or default to 3000
+    port: process.env.PORT || 3000, // Use PORT from environment or default to 3000
+    allowedHosts: [
+      'finance-planner-q8nv.onrender.com',
+      '.onrender.com' // This will allow all subdomains on onrender.com
+    ]
   }
 })
