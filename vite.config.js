17c1cc4918c513e9e7cc544920ce1a50185421c0
Riddh Modi <riddhmodi2003@gmail.com>
2025-03-11 09:21:50 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Needed for docker/render.com
+    strictPort: true,
+    port: process.env.PORT || 3000 // Use PORT from environment or default to 3000
+  }
 })
